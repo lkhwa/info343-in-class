63f1a19538123dc17a6a6e8ada8d2724805ca071
Leanne Hwa <lkhwa@uw.edu>
2016-10-18 16:44:38 -0700

    added animations for first five boxes

--- lab/dom/js/main.js
+++ lab/dom/js/main.js
@@ -6,3 +6,38 @@ var boxOne = document.getElementById('one'),
 	boxFour = document.getElementById('four'),
 	boxFive = document.getElementById('five'),
 	boxSix = document.getElementById('six');
+
+boxOne.addEventListener("click", function() {
+	boxOne.classList.add("fade-to-black");
+	boxOne.classList.add("black");
+})
+
+boxTwo.addEventListener("click", function() {
+	boxTwo.classList.add("fall");
+})
+
+boxThree.addEventListener("mouseover", function() {
+	boxThree.classList.add("fade-to-white");
+	boxThree.classList.add("white");
+})
+
+boxThree.addEventListener("mouseout", function() {
+	boxThree.classList.add("fade-to-bg-color");
+	boxThree.classList.remove("white");
+	boxThree.classList.add("grey");
+})
+
+boxFour.addEventListener("click", function() {
+	boxFour.classList.toggle("fall");
+})
+
+boxFive.addEventListener("click", function() {
+	var explode = setInterval(function() {
+		boxFive.style.height = (boxFive.offsetHeight + 3) + "px";
+		boxFive.style.width = (boxFive.offsetWidth + 3) + "px";
+		if (boxFive.offsetWidth >= 500) {
+			boxFive.style.display = "none";
+			clearInterval(explode);
+		}
+	});
+})
