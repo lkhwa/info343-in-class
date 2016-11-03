c2001ef15f98e67267c826ed637a2e293aadc5dc
Leanne Hwa <lkhwa@uw.edu>
2016-11-03 11:30:48 -0700

    babynames but with react

--- react-data/js/app.jsx
+++ react-data/js/app.jsx
@@ -1,19 +1,62 @@
 "use strict";
 
+/*var females = BABYNAMES.filter(function(record) {
+    return "F" == record.sex;
+});
+
+console.log(females.length);*/
+
+//ES6 can make one-line anon functions more concise:
+
+var females = BABYNAMES.filter(record => "F" == record.sex);
+
+//if you have one parameter, can delete extra ()
+//you also don't need the {} and return statement!
+
+console.log(females.length);
+
+//big arrow syntax for sort too!
+
+var topFemNames = females.sort((rec1, rec2) => rec2.count - rec1.count).slice(0,100);
+
+console.log(topFemNames.length);
+
+
+
+
 //main application React component
 class App extends React.Component {
     constructor(props) {
         super(props);
     }
 
+    //called by React framework to render content on page
+    //the React syntax removes the need to do document.createElement
+
     render() {
+        var colMeta = {
+            count: {
+                type: columnTypes.numeric,
+                caption: "Num of Babies"
+            }
+        }
+
         return (
             <div className="container">
-                <h1>Hello React!</h1>
+                <h1>Most Popular Female Baby Names from 1996</h1>
+
+                <DataTable records={this.props.records}     columnMeta={colMeta}/>
             </div>
         );
     }
 }
 
 //render the App component to the element with id="app"
-ReactDOM.render(<App/>, document.getElementById("app"));
+ReactDOM.render(<App records={topFemNames}/>, document.getElementById("app"));
+
+
+/*<ul>
+    {
+        this.props.records.map(record => <li key={record.name}>{record.name} {record.count}</li>)
+    }
+</ul>*/
